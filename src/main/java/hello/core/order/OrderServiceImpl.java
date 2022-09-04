5bc0c0f47f7496f81fbc41ea904de784b84bf7de
dainshon <shon1107@naver.com>
2022-09-04 21:11:04 +0900

    order

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);    //주문 생성이 되면 회원정보 조회하고
+        int discountPrice = discountPolicy.discount(member, itemPrice); //할인금액 가져옴
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
