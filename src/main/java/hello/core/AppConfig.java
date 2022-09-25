e94a8e196118293b34c4484ca190ad4c48168283
dainshon <shon1107@naver.com>
2022-09-25 14:45:40 +0900

    spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,24 +8,30 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 //AppConfig에서 실제 동작에 필요한 구현 객체 생성
 //AppConfig는 공연 기획자
+@Configuration
 public class AppConfig {
+    //App의 구성, 설정 정보에 @Configuration
+    //@Bean은 컨테이너에 등록됨.
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(MemberRepository());
     }
-
-    private MemberRepository MemberRepository() {
+    @Bean
+    public MemberRepository MemberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     public OrderService orderService(){
         //생성자 이용
         return new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy());
     }
-
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new FixDiscountPolicy();
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -1,15 +1,22 @@
 package hello.core;
 
 import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     private static MemberRepository memberRepository;
 
     public static void main(String[] args) {    //psvm 치고 엔터누르면됨.싱기
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-//        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //얘가 spring container
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //"memberService"가 이름, MemberService는 타입.
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
