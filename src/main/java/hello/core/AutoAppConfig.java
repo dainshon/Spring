2fac0692c37509d3e4525c8f6d1dffd1f66fc3ba
dainshon <shon1107@naver.com>
2022-11-15 21:19:36 +0900

    @ComponentScan

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan  //@Component 붙은 애들 다 찾아서 자동으로 빈에 등록해줌
         (
+                basePackages =  "hello.core.member",    //hello.core.member가 component 대상
                 excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class)  //얘는 제외
         )
 public class AutoAppConfig {
