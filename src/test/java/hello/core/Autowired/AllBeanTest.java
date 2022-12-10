eb9cb2da4411a844d630582aaf4044a5120cbf12
dainshon <shon1107@naver.com>
2022-12-10 15:27:32 +0900

    allbean

---
+++ src/test/java/hello/core/Autowired/AllBeanTest.java
@@ -0,0 +1,27 @@
+package hello.core.Autowired;
+
+import hello.core.discount.DiscountPolicy;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class AllBeanTest {
+    @Test
+    void findAllBean(){
+        new AnnotationConfigApplicationContext();
+
+    }
+
+    static class DiscountService{
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies){
+            this.policyMap = policyMap;
+            this.policies = policies;
+        }
+    }
+
+}
